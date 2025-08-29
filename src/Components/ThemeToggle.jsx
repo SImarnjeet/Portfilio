7cf3ee2b0f7aa3eae17f39fc50dea1a7bcc4d0f8
simranjeet <simranjeet77400@gmail.com>
2025-08-29 13:17:53 +0530

    rename

--- src/Components/ThemeToggle.jsx
+++ src/Components/ThemeToggle.jsx
@@ -1,6 +1,6 @@
 import {Moon,Sun} from "lucide-react"
 import { useEffect, useState } from "react";
-import { cn } from "../lib/Utils";
+import { cn } from "../Lib/Utils";
 
 
  const ThemeToggle = () => {
